fb870ed3f14d606481264b094b2eeed3379c8613
Mikhaela Dietch <Mikhaela@Mikhaelas-MacBook-Pro.local>
2016-03-08 23:11:38 -0500

    make a wish

--- setup.py
+++ setup.py
@@ -6,11 +6,9 @@ import glob
 import math
 from pygame import gfxdraw
 
-# links:
-# https://www.cs.ucsb.edu/~pconrad/cs5nm/topics/pygame/drawing/
-
-
-class Point(object):
+class Point(object): 
+    """ creating point class, to determine location of all our stuff 
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -18,15 +16,17 @@ class Point(object):
     def pos(self):
         return self.x, self.y
 
-
 class Vector(object):
+    """ creating motion vectors class that describe movement of points
+    """
     def __init__(self, magnitude, theta):
         self.m = magnitude
         self.t = theta
 
-
-#  a lot is being added here
 class Model(object):
+    """ our Model class generates and edits body attributes, bodies are defined by points
+    """
+
     # inits a custom number of bodies at semi-random positions inside screen
     def __init__(self, num_bodies=3, body_rad=10):
         self.bodies = []
@@ -34,28 +34,36 @@ class Model(object):
         global screen_size
         screen_width, screen_height = screen_size
 
-        while len(self.body_centers) < num_bodies:
-            x = random.randint(0+body_rad, screen_width-body_rad)
-            y = random.randint(0+body_rad, screen_height-body_rad)
-            print x, y
-            new_center = Point(x, y)
-            if not self.too_close(new_center, self.body_centers, 2*body_rad):
-                self.body_centers.append(new_center)
+        self.body_centers = [
+            Point(125, 125),
+            Point(375, 125),
+            Point(125, 375),
+            Point(375, 375)]
 
+        # creates list of center points for every body 
+        # while len(self.body_centers) < num_bodies:
+        #     x = random.randint(0+body_rad, screen_width-body_rad)
+        #     y = random.randint(0+body_rad, screen_height-body_rad)
+        #     print x, y
+        #     new_center = Point(x, y)
+        #     if not self.too_close(new_center, self.body_centers, 2*body_rad):
+        #         self.body_centers.append(new_center)
+
+        # creates bodies at each center 
         for center in self.body_centers:
             self.bodies.append(Body(center.pos(), body_rad))
 
         print self.body_centers
 
-        # self.body = Body((size[0]/2, size[1]/2), 50)
-
     def update(self):
+        """ Updates all object to prep for draw
+        """
         for body in self.bodies:
             body.update()
 
     def too_close(self, new_point, points, distance):
-        '''checks to see if the new_point is too close to any points in points
-        '''
+        """ Checks to see if the new_point is too close to any points in points
+        """
         if not points:
             return False
         for point in points:
@@ -66,35 +74,44 @@ class Model(object):
                 return True
         return False
 
-
 class Body(object):
+    """ Defines bodies with center points, and dimensions,
+        and initilizes behavior objects
+    """
     def __init__(self, pos, rad, vel=(0.5, 0.0)):
         '''pos is cartesian, vel is (magnitude, theta)'''
         self.rad = int(rad)
         self.center = Point(*pos)
         self.vel = Vector(*vel)
         self.vel.base_vel = vel[0]
-        # TODO
         self.vel.t = random.uniform(0, 2*math.pi)
         self.acc = Vector(0.0, 0.0)
-        self.animate = True
+        self.animate = False
         self.p_center = Point(*pos)
         self.next_positions = []
         self.flag = -1
 
-    # ball will move with constant velocity, smoothly varying direction
     def update(self):
+        """ This keeps all bodies on screen 
+            as it moves them. Particular movements 
+            for certain events are defined here
+        """
+        # debugging remenants: 
         # if self.flag != -1:
         #     import pdb; pdb.set_trace()
 
+        # drifing animation only if our flag is down (= -1)
+        # flag always begins as down
+        # currently flag is raised when B-key is recognized as 
+        # pressed in PygameKeyboardController objects
         if self.animate and self.flag == -1:
 
             self.acc.t = (3*self.acc.t + random.uniform(-0.1, 0.1)) / 4
             self.vel.t += self.acc.t
 
-            # TODO Fix!!!
             global screen_size
             screen_width, screen_height = screen_size
+
             if self.center.x < 0 + self.rad:
                 self.vel.t = math.pi - self.vel.t
                 self.p_center.x = float(0 + self.rad)
@@ -111,16 +128,22 @@ class Body(object):
             self.p_center.x += self.vel.m * math.cos(self.vel.t)
             self.p_center.y += self.vel.m * math.sin(self.vel.t)
 
-            self.vel.m = (7*self.vel.m + self.vel.base_vel)/8
+            self.vel.m = (7*self.vel.m + self.vel.base_vel)/8 # playing with magnitue of vel 
+        # catches case where animate is off but no flag
         elif self.flag == -1:
             pass
+        # frame_rate is used to determine length of behavior given B-key press 
+        # this keeps the flag raised until we reach the end of our framerate
         elif self.flag >= frame_rate -1:
             self.flag = -1
+        # when flag raised
         else:
+            # debuggy stuff
             # import pdb; pdb.set_trace()
             self.p_center.x, self.p_center.y = self.next_positions[self.flag]
             self.flag += 1
 
+        # finally round and set center coords
         self.center.x = int(round(self.p_center.x))
         self.center.y = int(round(self.p_center.y))
 
@@ -131,23 +154,28 @@ class Body(object):
 
 
 class PyGameWindowView(object):
-    def __init__(self, model, screen):
-        # self.model = model
+    """ This puts everything on a scree
+        So you can put your eyeballs on it
+        and play, or whatever. 
+    """
+    def __init__(self, screen):
         self.screen = screen
 
     def draw(self, model):
+        """ Draw background and all bodies
+        """
         # paint background
-        self.screen.fill((200, 200, 200))  # (255, 32, 103))
+        self.screen.fill((20, 20, 20))  # (255, 32, 103))
 
         # draw every body in bodies
         for body in model.bodies:
             circle_png = pygame.image.load('circle.png')
-            # circle = pygame.transform.scale(circle_png, (10,10))
             self.screen.blit(circle_png, 
                             (body.center.x -5,
                             body.center.y -6 )
                             )
-
+        # brings in our list of pos for each body center 
+        # and then we draw bodies 
         centers_list = [body.center.pos() for body in model.bodies]
 
         # draw joining lines
@@ -162,6 +190,8 @@ class PyGameWindowView(object):
 
 
 class PyGameAudio(object):
+    """ This is where the audio is loaded
+    """ 
     def __init__(self):
         # get paths for all .wavs in sounds flolder
         self.sound_paths = glob.glob('sounds/*.wav')
@@ -173,6 +203,8 @@ class PyGameAudio(object):
 
 
 class PyGameKeyboardController(object):
+    """ Where loaded audio is mapped to keys 
+    """ 
     def __init__(self, model, audio_unit):
         self.model = model
         self.snap_vertical = True
@@ -200,7 +232,10 @@ class PyGameKeyboardController(object):
             # import pdb; pdb.set_trace()
             for body in model.bodies:
             	body.flag = 0
+                # print body.vel.t, 'init theta'
+                # print body.center.pos(), 'init pos'
                 body.next_positions = self.get_pos_list(body) # pulls the list of coming postition for a node
+                # print body.next_positions
                 
         #  space quits for speed in testing
         elif event.key == K_SPACE:
@@ -213,32 +248,66 @@ class PyGameKeyboardController(object):
         """ Calculates a list of future positions for a node
             given the event that B-key is pressed
         """ 
+
         global frame_rate
         pos_list = [] # empty list for all our postition for a node 
-        center_screen = Point(250,250) #just using center of screen for now, avg center next
-        body.vel.t = math.atan((body.center.x - center_screen.x)/(body.center.y - center_screen.y))
-        frame_list = range(1,frame_rate) # will be populated with our pos val for B-key event
+        # TODO move somewhere better
+        print body.center.pos()
+
+        avg = [0, 0]
+        for body in self.model.bodies:
+            avg[0] = avg[0] + body.center.x
+            avg[1] = avg[1] + body.center.y
+
+        avg = [avg[0]/len(self.model.bodies), avg[1]/len(self.model.bodies)]
+        print avg
+        print body.center.x, body.center.y
+
+        x = sum([body.center.x for body in self.model.bodies])/len(self.model.bodies)
+        y = sum([body.center.y for body in self.model.bodies])/len(self.model.bodies)
+        # print body.center.pos()
+
+        center_mass = Point(x,y) 
+
+        # if body.center.x < center_mass.x:
+        #     marker = -1
+        # else:
+        #     marker = 1
+
+        # TODO check signs
+        theta = math.atan(float(center_mass.y - body.center.y)/(body.center.x - center_mass.x))
+        if body.center.x < center_mass.x:
+            theta += math.pi / 2
+        print theta, body.center.pos()
+
+        frame_list = range(0,frame_rate) # will be populated with our pos val for B-key event
         now_pos = body.center.pos() # pull the immediate location of the current node
         for frame in frame_list: 
             dist = self.pos_curve(frame,frame_rate) # get dist of movement for origial pos 
-            dy = dist * math.sin(body.vel.t) # change in y pos
-            dx = dist * math.cos(body.vel.t) # change in x pos 
+            dy = dist * math.sin(theta) # change in y pos
+            dx = dist * math.cos(theta) # change in x pos 
             pos_list.append(tuple(map(sum, zip(now_pos, (dx, dy)))))
+        # print pos_list
         return pos_list
 
     def pos_curve(self, frame, frame_rate):
+        """ makes the bodies move nice...eventually 
+        """
         dec = 0.137129
-        frame_div = float(frame) / (frame_rate*5)
-        dist = ( (math.log10(5*(frame_div + dec)) + 1) / (5*(frame_div + dec)) - 1 ) * 500
+        frame_div = float(frame) / (frame_rate)
+        dist = ( (math.log10((frame_div + dec)) + 1) / ((frame_div + dec)) - 1 ) * 500
         return dist
 
     def speed_random(self, model, velocity=20.0, spin=1.0):
+        """ chooses a random body and gives it a particular speed
+        """ 
         target = random.choice(model.bodies)
         target.vel.m = velocity
         target.acc.t = spin * random.choice((1, -1))
 
 
 if __name__ == '__main__':
+
     try:
         pygame.quit()
     except:
@@ -251,7 +320,7 @@ if __name__ == '__main__':
 
     model = Model(1, 0)
     audio_unit = PyGameAudio()
-    view = PyGameWindowView(model, background)
+    view = PyGameWindowView(background)
     controller = PyGameKeyboardController(model, audio_unit)
 
     running = True
